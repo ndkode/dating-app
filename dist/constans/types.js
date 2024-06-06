91ce7a31fd2d2e8e012970a77bb732101f23f6d4
Nandang Kurniawan <nandang.k95@gmail.com>
2024-06-06 19:42:15 +0700

    Remove dist folder

--- dist/constans/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-exports.TYPES = {
-    UserController: Symbol.for("UserController"),
-    UserService: Symbol.for("UserService"),
-    UserRepository: Symbol.for("UserRepository"),
-};
